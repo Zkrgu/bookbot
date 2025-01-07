5ee9d31cf73fa7c9cd7c88432b58f56a4bd89f01
Jonas Krogh <xyzkrgu@gmail.com>
2025-01-07 19:45:02 +0100

    Print book report

--- main.py
+++ main.py
@@ -11,13 +11,25 @@ def char_counts(string):
 def count_words(string):
     return len(string.split())
 
-def main():
-    with open("books/frankenstein.txt") as f:
+def print_report(book, words, char_counts):
+    print(
+            f"--- Begin report of {book} ---\n"
+            f"{words} words found in the document\n"
+    )
+    for char, count in sorted(char_counts.items(), key=lambda item: item[1])[::-1]:
+        print(f"The '{char}' character was found {count} times")
+
+    print('--- End report ---')
+
+def print_report_for_book(location):
+    with open(location) as f:
         file_contents = f.read()
         file_words = count_words(file_contents)
         character_counts = char_counts(file_contents)
-        print(file_words)
-        print(character_counts)
+        print_report(location, file_words, character_counts)
+
+def main():
+    print_report_for_book("books/frankenstein.txt")
 
 if __name__ == "__main__":
     main()
