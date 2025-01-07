655ed1d03261129421982b41f6c0a1ece1bd2e67
Jonas Krogh <xyzkrgu@gmail.com>
2025-01-07 19:36:15 +0100

    Count character occurences

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-def count_words(str):
-    return len(str.split())
+def char_counts(string):
+    chars = {}
+    for char in string.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
+
+
+def count_words(string):
+    return len(string.split())
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         file_words = count_words(file_contents)
+        character_counts = char_counts(file_contents)
         print(file_words)
+        print(character_counts)
 
 if __name__ == "__main__":
     main()
